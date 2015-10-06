c57c4c2e1b210266013f396406fe38baa4b4117a
Shaun <discospider@me.com>
2015-10-06 10:01:39 -0400

    Issue #5 : clean up

--- bash_profile.sh
+++ bash_profile.sh
@@ -13,11 +13,10 @@ tagSym=$(echo -e "\xE1\x9A\xB9")
 #
 
 findDotFiles() {
-#http://content.hccfl.edu/pollock/Unix/FindCmd.htm
+# http://content.hccfl.edu/pollock/Unix/FindCmd.htm
 # find ~ -maxdepth 5 -name "*dotfiles" | sort -f -u -d -r | head -1
 if dotPath="`find ~ -maxdepth 3 -iregex '^.*dotfiles2' | sort -f -u -d -r | head -1`"
 then
-#   echo $dotPath
     export dotPath
 else
     echo "Failed to find dotPath."
@@ -31,7 +30,6 @@ gitMeta() {
 
 	gitHash="$(git --git-dir $dotPath/.git --work-tree=$dotPath log -n 1 | grep -m1 "commit [a-z0-9]" | awk '{ print substr($2,1,7) }')"
 
-	# override VERBASH if we can determine a tag
 	gitTag="$(git --git-dir $dotPath/.git --work-tree=$dotPath describe --tags --always)"
 
   if [[ "$gitTag" -eq "$gitHash" ]]
@@ -51,7 +49,6 @@ findDotFiles
 
 # find and print git meta data
 gitMeta
-## unset some of the large functions that won't be needed once processed
 
 ###
 ## process other files
@@ -71,7 +68,6 @@ fi
 
 unset findDotFiles
 unset gitMeta
-unset gitWTag
 unset gitTag
 unset gitHash
 unset gitBranch
